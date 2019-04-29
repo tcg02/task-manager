edcede05ddc7461012b9b0ed279f6c4931c6f79d
sdahal <sdahal@wtop.com>
2019-04-29 15:31:53 -0400

    promise chaining

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -10,3 +10,15 @@ const Task = require("../src/models/task")
 //     console.log(e)
 // })
 
+const deleteTaskAndCount = async (id)=>{
+   const task = await Task.findByIdAndDelete(id)
+   const count = await Task.countDocuments({completed:false})
+   return count
+}
+
+
+deleteTaskAndCount('5cc732e95c627d2cf8dfdfee').then((count)=>{
+    console.log(count)
+}).catch((e)=>{
+    console.log(e)
+})
