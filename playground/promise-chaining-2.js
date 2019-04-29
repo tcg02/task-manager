34dda073e94ab21cbd126f65dae04aab912f9170
sdahal <sdahal@wtop.com>
2019-04-29 15:22:09 -0400

    Mongoose api with Promise async and awit

---
+++ playground/promise-chaining-2.js
@@ -0,0 +1,12 @@
+require("../src/db/mongoose")
+const Task = require("../src/models/task")
+
+// Task.findByIdAndDelete('5cc7321b919bf42bbcf38d0f').then((task)=>{
+//     console.log(task)
+//     return Task.countDocuments({completed:fasle})
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,27 +1,8 @@
 const mongoose = require('mongoose')
+ 
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true,
-    useCreateIndex: true
-})
-
-const User = mongoose.model('Users',{
-    name: {
-        type: String
-    },
-    age:{
-        type: Number
-    }
-})
-
-
-const me = new User({
-    name:"Santosh", 
-    age: 31
-})
-
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log("Error", error)
+    useCreateIndex: true,
+    useFindAndModify: false
 })
